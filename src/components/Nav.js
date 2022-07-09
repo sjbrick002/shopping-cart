0417ced3499fe2062a9b17c18ba645210526738f
SBrick <samuelbrick3@gmail.com>
2022-07-09 17:20:15 -0500

    Added Links to the Nav component

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,15 @@
+import { Link } from "react-router-dom";
+
 export default function Nav(props) {
     let pagesArray = props.pages;
     return (
         <nav>
             <ul>
-                {pagesArray.map(page => <li key={page.toLowerCase()}>{page}</li>)}
+                {pagesArray.map(page => 
+                    <li key={page.toLowerCase()}>
+                        <Link to={`/${page.toLowerCase()}`}>{page}</Link>
+                    </li>
+                )}
             </ul>
         </nav>
     );
--- src/components/Nav.test.js
+++ src/components/Nav.test.js
@@ -1,10 +1,15 @@
 import { screen, render } from "@testing-library/react"
 import Nav from "./Nav";
+import { BrowserRouter } from "react-router-dom";
 
 describe("Nav component", () => {
     it("renders a number of list items equal to the array length of the passed pages prop", () => {
         let pages = ["Home", "Shop", "Checkout"];
-        render(<Nav pages={pages}/>);
+        render(
+            <BrowserRouter>
+                <Nav pages={pages}/>
+            </BrowserRouter>
+        );
         expect(screen.getAllByRole("listitem").length).toEqual(pages.length)
     });
 });
